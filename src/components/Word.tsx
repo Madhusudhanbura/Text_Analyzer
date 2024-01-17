c2f1e9436f421feb8f0b267e98c886f624424668
Madhusudhanbura <66729203+Madhusudhanbura@users.noreply.github.com>
2024-01-17 23:32:17 +0530

    Update Word.tsx

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -9,16 +9,35 @@ const Word = () => {
   const { characterCount, wordCount } = calculateTextMetrics(text);
 
   return (
-    <div className="flex w-full flex-col mt-3">
-      <input
-        type="text"
-        autoFocus
-        className="h-10 mb-4 px-3 py-2 rounded-sm focus:none outline-none"
-        placeholder="Start typing here..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <WordTable words={wordCount} characters={characterCount} />
+    <div className="flex  flex-col mt-3">
+      <div className="flex gap-4 mb-4 items-center ">
+        <input
+          type="text"
+          autoFocus
+          value={text}
+          placeholder="Start typing here..."
+          className="h-10  px-3 w-10/12 py-2 border border-[#D2D6DB] rounded-sm focus:none outline-none"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className="bg-[#731EE2] text-[#FFFFFF] px-3 py-2 font-sans font-normal rounded-sm  text-sm">
+          Process Word
+        </button>
+      </div>
+      <WordTable words={words} characters={characters} />
+      <div className="w-full p-3 flex flex-col border border-[#D2D6DB] mt-4">
+        {data.map((row, index) => {
+          return (
+            <div key={index} className="flex gap-4 w-full justify-start">
+              <div className="font-sans font-medium min-w-[100px] w-1/12 text-base text-[#475467]">
+                {row.name}
+              </div>
+              <div className="text-[#111927] font-sans font-medium text-base">
+                {row.value}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
